0f61096e740de900ff122a05348af08a19b5ad00
matheusopb <30798663+matheusopb@users.noreply.github.com>
2021-08-28 18:20:29 -0300

    Primeira versão ainda falta terminar alguns testes

--- src/api/database/index.js
+++ src/api/database/index.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/Cookmaster', {
+mongoose.connect('mongodb://mongodb:27017/Cookmaster', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-  }).then(() => {
-    console.log('Database/index.js DB connected !');
-  }).catch((err) => {
-    console.log(`Database/index.js error connecting to DB! ${err}`);
   });
 
 mongoose.Promise = global.Promise;
--- src/integration-tests/userService.test.js
+++ src/integration-tests/userService.test.js
@@ -3,7 +3,8 @@ const userService = require('../api/service/userService');
 const should = chai.should();
 const { MongoClient } = require('mongodb');
 
-const mongoDbUrl = 'mongodb://localhost:27017/Cookmaster';
+const mongoDbUrl = 'mongodb://mongodb:27017/Cookmaster';
+
 
 
 describe('Teste da funcao addUser', () => {
