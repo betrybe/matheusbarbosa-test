e932476900bb3f530372115f0b34b403f6fd263b
matheusopb <30798663+matheusopb@users.noreply.github.com>
2021-09-01 16:40:35 -0300

    Alterando banco para avaliado

--- src/api/database/index.js
+++ src/api/database/index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://mongodb:27017/Cookmaster', {
+  
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    useCreateIndex: true, 
+
   });
 mongoose.Promise = global.Promise;
 
